ec91a74c55525929a8f72056da8b1cfebac0cadf
unknown <Saitharala@gmail.com>
2021-11-12 15:28:49 +0530

    [ Tharala SaiPrashanth] Added Flipcoin Feauture

---
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an Option To Execute the Program\n 1.flipcoin\n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Flipcoin coin = new Flipcoin();
+                        coin.Play();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
