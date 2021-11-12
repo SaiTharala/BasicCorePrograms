6e1da0bf12941373abf2d8b329d20344f5db236f
unknown <Saitharala@gmail.com>
2021-11-12 16:04:24 +0530

    [ Tharala SaiPrashanth] Added PowerofTwo Feauture

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -9,7 +9,7 @@ namespace BasicCoreProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose an Option To Execute the Program\n 1.flipcoin\n 2.LeapYearCheck\n 3.Exit");
+                Console.WriteLine("Choose an Option To Execute the Program\n 1.flipcoin\n 2.LeapYearCheck\n 3.PowerOfTwo\n 4.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -22,6 +22,10 @@ namespace BasicCoreProgram
                         check.LeapYearCheck();
                         break;
                     case 3:
+                        PowerofTwo pow = new PowerofTwo();
+                        pow.Power();
+                        break;
+                    case 4:
                         flag = false;
                         break;
                     default:
